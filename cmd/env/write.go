b68ba7c0a0d5f7a0864e3a1b2530c5cd7b3e8eb9
enta0701 <endo.takuya.0701@gmail.com>
2017-12-11 11:14:20 +0900

    Use bufio.NewWriter

--- cmd/env/write.go
+++ cmd/env/write.go
@@ -1,12 +1,16 @@
 package env
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func (c *Config) Write(f *os.File) {
-	fmt.Fprintf(f, `package %s
+	w := bufio.NewWriter(f)
+	defer w.Flush()
+
+	fmt.Fprintf(w, `package %s
 
 func Get(k string) string {
 	return List()[k]
@@ -16,11 +20,11 @@ func List() map[string]string {
 	return map[string]string{`, c.PackageName)
 
 	for k, v := range c.Data {
-		fmt.Fprintf(f, `
+		fmt.Fprintf(w, `
 		"%s": "%s",`, k, v)
 	}
 
-	fmt.Fprint(f, `
+	fmt.Fprint(w, `
 	}
 }`)
 }
